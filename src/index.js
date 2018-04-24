cbc5ff48577897e00256693bfd34e57c29db3eac
Glitch (hello-world-graphql-client) <none>
2018-04-24 14:49:11 +0000

    🚤🍮 Checkpoint ./index.js:998190/858 ./index.html:998190/2419 ./package.json:998190/433 ./watch.json:998190/241 ./src/index.js:998190/108

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+import fetch from "unfetch";
+
+const API_URL = "https://hello-world-graphql.glitch.me"
+
+class Greet extends Component {
+  render() {
+    return <div>Hello {this.props.to}</div>;
+  }
+}
+
+export default class App extends Component {
+  state = { to: "" };
+  componentDidMount() {
+    fetch(`${API_URL}/graphql`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ query: "{ planet }" })
+    })
+      .then(res => res.json())
+      .then(res => this.setState({ to: res.data.planet }));
+  }
+  render() {
+    return <Greet to={this.state.to} />;
+  }
+}
+
+render(<App />, document.getElementById("root"));
