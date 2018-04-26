208c5f4a35a9e3a16e3ba0ca0cd9ea2e2609c7a6
Glitch (gamy-advertisement) <none>
2018-04-26 14:46:27 +0000

    😅🌆 Checkpoint ./server.js:998190/1353 ./server/index.js:998190/84 ./server/database.js:998190/100 ./server/knexfile.js:998190/1283 ./package.json:998190/11581 ./server/schema.js:998190/1204 ./server/Server.js:998190/6

---
+++ server/knexfile.js
@@ -0,0 +1,70 @@
+module.exports = {
+  test: {
+    client: "postgresql",
+    connection: {
+      host: process.env.DBHOST,
+      database: "pinapp-test",
+      user: process.env.DBUSER,
+      password: process.env.DBPASSWORD
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations"
+    }
+  },
+
+  development: {
+    client: "postgresql",
+    connection: {
+      host: process.env.DBHOST,
+      database: process.env.DB,
+      user: process.env.DBUSER,
+      password: process.env.DBPASSWORD
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations"
+    }
+  },
+
+  staging: {
+    client: "postgresql",
+    connection: {
+      host: process.env.DBHOST,
+      database: process.env.DB,
+      user: process.env.DBUSER,
+      password: process.env.DBPASSWORD
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations"
+    }
+  },
+
+  production: {
+    client: "postgresql",
+    connection: {
+      host: process.env.DBHOST,
+      database: process.env.DB,
+      user: process.env.DBUSER,
+      password: process.env.DBPASSWORD,
+      ssl: true
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations"
+    }
+  }
+};
