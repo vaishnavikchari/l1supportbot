f73602d86f0c3eacd73670af5e97e1e68effdcaf
Glitch (react-app-node-api) <none>
2019-08-20 05:00:55 +0000

    🌚🗽 Checkpoint ./server.js:4964900/1429 ./package.json:4964900/2274

--- server.js
+++ server.js
@@ -54,6 +54,56 @@ server.use(
   })
 );
 
+//dialogflow
+process.env.GOOGLE_APPLICATION_CREDENTIALS = __dirname + '/.data/wordofbot-8afdc07a2a39.json';
+
+server.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+
+server.get("/df", (req, res, next) => {
+    var q = req.query.q,
+    session = req.query.sessionId;
+    
+    runQuery(q, res, session);
+});
+
+/**
+ * Send a query to the Dialogflow agent, and return the query result.
+ * @param {string} q The question
+ * @param {object} res The object to reply the answer
+ * @param {string} sId The session identifier
+ */
+async function runQuery(q, res, sId) {
+    // A Dialog Flow / Google Project ID:
+    const pId = "wordofbot";
+    // Create a new session
+    const sessionClient = new dialogflow.SessionsClient();
+    const sessionPath = sessionClient.sessionPath(pId, sId);
+
+    // The text query request.
+    const request = {
+        session: sessionPath,
+        queryInput: {
+            text: {
+                text: q,
+                languageCode: 'en-US',
+            },
+        },
+    };
+
+    // Send request and log result
+    const responses = await sessionClient.detectIntent(request);
+    const result = responses[0].queryResult;
+
+    // Send Result if string.indexOf(substring) !== -1
+    res.send(result.fulfillmentMessages[0].text.text[0]);
+  
+}
+
 server.listen(PORT, () => {
   console.log(`Go to http://${HOST}:${PORT}/ to run queries!`);
 });
